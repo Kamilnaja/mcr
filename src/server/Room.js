00fdbff3a7f0b91354d4a66a4dd78df41310bc30
najak <kamilnaja@gmail.com>
2019-10-05 23:10:36 +0200

    remove unused code

--- src/server/Room.js
+++ src/server/Room.js
@@ -19,16 +19,6 @@ class Room {
     return this._usersIds();
   }
 
-  addUser(id) {
-    this._usersIds.push('hello');
-    console.log('adding ');
-    console.log(id);
-
-    if (this.checkIfUserIsAlreadyAdded()) {
-      return;
-    }
-  }
-
   checkIfUserIsAlreadyAdded() {
     if (this._usersIds.some(utils.findById)) {
       console.warn('User with this id is currently in given room');
